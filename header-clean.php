b3c8d7493e5595b61025d6c87cbfd5a5ea019f96
Leighton <shiasapir@gmail.com>
2018-04-04 08:29:32 -0500

    [New] Header for coming soon page

---
+++ header-clean.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section and everything up
+ *
+ * @package YADO Theme
+ * @since 1.0
+ * @version 1.0
+ */
+?><!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+	<head>
+		
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<?php wp_head(); ?>
+
+	</head>
+
+	<body <?php body_class( 'yui' ); ?>>
