f51e01656788987ccc058f7277432e1aab0b9584
Leighton <shiasapir@gmail.com>
2018-04-03 23:12:32 -0500

    [Update] Split content for logged-in and logged-out users

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php if ( is_user_logged_in() ) {
+
+/* Silence is golden */
+
+} else {
+
+/**
+ * 
+ * Coming Soon
+ * 
+ * Display "coming soon" page for non-logged-in users
+ * to create expectation before launch.
+ * 
+ * Allow users to subscribe YADO newsletter.
+ * 
+ */
+
+} ?>
