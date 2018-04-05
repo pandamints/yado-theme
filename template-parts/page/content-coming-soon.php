7d099f90942988bbdc3f5cd5b836c251fa0feb1d
Leighton <shiasapir@gmail.com>
2018-04-05 09:18:17 -0500

    [Update] Coming soon page content

--- template-parts/page/content-coming-soon.php
+++ template-parts/page/content-coming-soon.php
@@ -1,3 +1,7 @@
-<h1><?php _e( 'Prepárate para descubrir tu ciudad de una manera diferente y divertida', 'yadoapp' ); ?></h1>
+<div class="yui-content">
+	
+	<h1><?php _e( 'Prepárate para descubrir tu ciudad de una manera diferente y divertida', 'yadoapp' ); ?></h1>
 
-<h2><?php _e( 'Suscríbete ahora y sé el primero en enterarte de nuestro lanzamiento', 'yadoapp' ); ?></h2>
+	<h2><?php _e( 'Suscríbete ahora y sé el primero en enterarte de nuestro lanzamiento', 'yadoapp' ); ?></h2>
+
+</div>
