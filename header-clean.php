72ae7d607b8460262b6c876889efe1787117547c
Leighton <shiasapir@gmail.com>
2018-04-23 09:41:05 -0500

    [Update] Coming soon header

--- header-clean.php
+++ header-clean.php
@@ -13,14 +13,14 @@
 <html <?php language_attributes(); ?>>
 
 	<head>
-		
+
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
+
 		<?php wp_head(); ?>
 
 	</head>
 
 	<body class="yui">
 
-		<div class="yui-wrap yui-gradient">
+		<div class="yui-wrap">
