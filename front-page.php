83230f1a4725fda06eea32af3f57169f144b7716
Leighton <shiasapir@gmail.com>
2018-04-23 09:04:52 -0500

    [phpcs] improvements

--- front-page.php
+++ front-page.php
@@ -12,18 +12,18 @@ if ( is_user_logged_in() ) {
 	get_header( 'clean' );
 
 	/**
-	 * 
+	 *
 	 * Coming Soon
-	 * 
+	 *
 	 * Display "coming soon" page for non-logged-in users
 	 * to create expectation before launch.
-	 * 
+	 *
 	 * Allow users to subscribe YADO newsletter.
-	 * 
+	 *
 	 */
-	
+
 	get_template_part( 'template-parts/page/content', 'coming-soon' );
 
 	get_footer( 'clean' );
 
-} ?>
+}
