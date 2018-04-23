e77b9d110b293a18a80a52e1a30be975b16bea3f
Leighton <shiasapir@gmail.com>
2018-04-23 09:05:28 -0500

    [update] coming soon template

--- template-parts/page/content-coming-soon.php
+++ template-parts/page/content-coming-soon.php
@@ -1,7 +1,9 @@
 <div class="yui-content">
-	
-	<h1><?php _e( 'Prepárate para descubrir tu ciudad de una manera diferente y divertida.', 'yadoapp' ); ?></h1>
 
-	<h2><?php _e( 'Suscríbete ahora y sé el primero en enterarte de nuestro lanzamiento.', 'yadoapp' ); ?></h2>
+	<h1><?php esc_html_e( 'Prepárate para descubrir tu ciudad de una manera diferente y divertida.', 'yadoapp' ); ?></h1>
+
+	<h2><?php esc_html_e( 'Suscríbete ahora y sé el primero en enterarte de nuestro lanzamiento.', 'yadoapp' ); ?></h2>
+
+	<p><?php echo do_shortcode( '[wd_hustle id="coming-soon" type="embedded"]' ); ?>
 
 </div>
