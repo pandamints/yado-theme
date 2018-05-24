779259e92a6114e9f89a530504923d83b9c8f20b
Leighton <leigh@incsub.com>
2018-05-24 09:41:02 -0500

    [Update] Thank you page template

---
+++ page-thanks.php
@@ -0,0 +1,14 @@
+<?php
+/***
+ * Template Name: Thank You
+ *
+ * @package Yado
+ * @subpackage Yado
+ * @since Yado 1.0
+ */
+
+get_header( 'clean' );
+
+get_template_part( 'template-parts/page/content', 'thank-you' );
+
+get_footer( 'clean' );
